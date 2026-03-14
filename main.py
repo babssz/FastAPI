5493f42491d06ec5ac8408db06486b010eca8eb3
unknown <salsabilachacha2411@gmail.com>
2026-03-14 23:12:46 +0800

    FastAPI items API

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+from database import engine, Base, SessionLocal
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/items/", response_model=list[schemas.Item])
+def get_items(db: Session = Depends(get_db)):
+    items = db.query(models.Item).all()
+    return items
+
+
+@app.get("/items/{item_id}", response_model=schemas.Item)
+def get_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    return item
---
+++ schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    name: str
+    description: str
+
+    class Config:
+        from_attributes = True
